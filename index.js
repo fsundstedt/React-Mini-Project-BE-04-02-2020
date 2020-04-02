a87b148501e117d46fe8d56edef734f89009dd45
Frank Sundstedt <frank.sundstedt@gmail.com>
2020-04-02 14:55:27 -0400

    attempted to resolve CORS issue.

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const express = require("express");
+const cors = require('cors');
 const Posts = require("./models/models.js")
 
 const PORT = 2000;
 const app = express();
 
+app.use(cors())
+
 async function pull() {
     const data = await Posts.getAll();
-    console.log(data[0].title)
+    console.log(data.title)
     return data;
 }
 
 console.log(pull())
 
-app.get("/hello", (req, res) => {
-    res.json(pull()).status(200);
+app.get("/posts", async function(req, res, next) {
+    const all = await Posts.getAll();
+    res.json(all);
   });
 
 app.listen(PORT, () => {
